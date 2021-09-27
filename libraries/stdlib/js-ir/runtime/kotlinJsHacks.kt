b48aa56e4f477cd28de742533b9b1cb4316af988
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2021-09-27 16:58:03 +0300

    [JS IR] Deprecate safeProperty(Get|Set) methods from stdlib

--- libraries/stdlib/js-ir/runtime/kotlinJsHacks.kt
+++ libraries/stdlib/js-ir/runtime/kotlinJsHacks.kt
@@ -13,14 +13,18 @@ internal fun <T : Enum<T>> enumValuesIntrinsic(): Array<T> =
 internal fun <T : Enum<T>> enumValueOfIntrinsic(@Suppress("UNUSED_PARAMETER") name: String): T =
     throw IllegalStateException("Should be replaced by compiler")
 
-@PublishedApi
-internal fun safePropertyGet(self: dynamic, getterName: String, propName: String): dynamic {
+// These were necessary for legacy-property-access functionality in IR
+// But this functionality is not required anymore, so these methods are redundant
+// But they can't be removed because if old version of compiler will compile against new version of stdlib
+// (when these methods are removed) compiler will fail with Internal error
+@Deprecated("This is intrinsic for removed functionality", level = DeprecationLevel.HIDDEN)
+private fun safePropertyGet(self: dynamic, getterName: String, propName: String): dynamic {
     val getter = self[getterName]
     return if (getter != null) getter.call(self) else self[propName]
 }
 
-@PublishedApi
-internal fun safePropertySet(self: dynamic, setterName: String, propName: String, value: dynamic) {
+@Deprecated("This is intrinsic for removed functionality", level = DeprecationLevel.HIDDEN)
+private fun safePropertySet(self: dynamic, setterName: String, propName: String, value: dynamic) {
     val setter = self[setterName]
     if (setter != null) setter.call(self, value) else self[propName] = value
 }
